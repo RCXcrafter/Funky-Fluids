29fae28f58f90340a6fbd54db3c20dd6233ec124
RCXcrafter <rcxcrafter@gmail.com>
2022-09-03 15:03:06 +0200

    fix oobleck and liquid crystal flowing where it shouldn't

--- src/main/java/com/rcx/funkyfluids/util/FunkyFluidsMaterials.java
+++ src/main/java/com/rcx/funkyfluids/util/FunkyFluidsMaterials.java
@@ -6,9 +6,9 @@ import net.minecraft.world.level.material.PushReaction;
 
 public class FunkyFluidsMaterials {
 
-	public static final Material OOBLECK_MATERIAL = new FunkyFluidsMaterials.Builder(MaterialColor.WOOL).notSolidBlocking().nonSolid().destroyOnPush().replaceable().liquid().build();
+	public static final Material OOBLECK_MATERIAL = new FunkyFluidsMaterials.Builder(MaterialColor.WOOL).noCollider().notSolidBlocking().nonSolid().destroyOnPush().replaceable().liquid().build();
 	public static final Material RED_FLUID_MATERIAL = new FunkyFluidsMaterials.Builder(MaterialColor.COLOR_RED).noCollider().notSolidBlocking().nonSolid().destroyOnPush().replaceable().liquid().build();
-	public static final Material LIQUID_CRYSTAL_MATERIAL = new FunkyFluidsMaterials.Builder(MaterialColor.COLOR_PURPLE).notPushable().liquid().build();
+	public static final Material LIQUID_CRYSTAL_MATERIAL = new FunkyFluidsMaterials.Builder(MaterialColor.COLOR_PURPLE).noCollider().notPushable().liquid().build();
 	public static final Material SILLY_PUTTY_MATERIAL = new FunkyFluidsMaterials.Builder(MaterialColor.COLOR_MAGENTA).notSolidBlocking().nonSolid().destroyOnPush().replaceable().liquid().build();
 	public static final Material PURPLE_FLUID_MATERIAL = new FunkyFluidsMaterials.Builder(MaterialColor.COLOR_PURPLE).noCollider().notSolidBlocking().nonSolid().destroyOnPush().replaceable().liquid().build();
 
