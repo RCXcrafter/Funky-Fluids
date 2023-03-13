a54f80a359e9ba9e1a80044ae50d1ba3ec568a9a
RCXcrafter <rcxcrafter@gmail.com>
2023-03-13 22:07:22 +0100

    allow buckets to interact with as fluid containers

--- src/main/java/com/rcx/funkyfluids/items/FunkyFluidsBucketItem.java
+++ src/main/java/com/rcx/funkyfluids/items/FunkyFluidsBucketItem.java
@@ -12,6 +12,8 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.fluids.capability.wrappers.FluidBucketWrapper;
 
 public class FunkyFluidsBucketItem extends BucketItem {
 
@@ -22,5 +24,10 @@ public class FunkyFluidsBucketItem extends BucketItem {
 	@Override
 	public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltip, TooltipFlag pIsAdvanced) {
 		pTooltip.add(Component.translatable(this.getDescriptionId() + ".lore").withStyle(ChatFormatting.GRAY));
+	}
+
+	@Override
+	public ICapabilityProvider initCapabilities(ItemStack stack, @org.jetbrains.annotations.Nullable net.minecraft.nbt.CompoundTag nbt) {
+		return new FluidBucketWrapper(stack);
 	}
 }
